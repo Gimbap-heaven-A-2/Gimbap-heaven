8e13280e5d0b0775ca73a98d940f59b13f63607e
jshstar <jshstar97@gmail.com>
2023-12-05 17:04:30 +0900

    gimbap_heaven init

---
+++ src/main/java/com/sparta/gimbap_heaven/GimbapHeavenApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.gimbap_heaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimbapHeavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimbapHeavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/gimbap_heaven/GimbapHeavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.gimbap_heaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimbapHeavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
