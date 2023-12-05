fe6a476e52592697895dc03561ff0c4ff0d16800
Kimhyeyun <cqqwer@gmail.com>
2023-12-05 19:29:31 +0900

    feat: Order Entity 클래스 추가

---
+++ src/main/java/com/sparta/gimbap_heaven/order/entity/Order.java
@@ -0,0 +1,22 @@
+package com.sparta.gimbap_heaven.order.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private boolean isOrdered = false;
+}
