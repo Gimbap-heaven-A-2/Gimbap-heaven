fedc31f298ba38b4f384fd7766d00fcff5e749db
Kimhyeyun <cqqwer@gmail.com>
2023-12-06 20:31:36 +0900

    feat: 권한 없는 경우 에러 코드 추가

--- src/main/java/com/sparta/gimbap_heaven/global/constant/ErrorCode.java
+++ src/main/java/com/sparta/gimbap_heaven/global/constant/ErrorCode.java
@@ -15,12 +15,6 @@ public enum ErrorCode {
     // System Exception
     INVALID_USER(HttpStatus.BAD_REQUEST,"일치하는 유저정보가 없습니다."),
 
-    INVALID_MENU(HttpStatus.BAD_REQUEST,"일치하는 메뉴 정보가 없습니다."),
-
-    INVALID_CART(HttpStatus.BAD_REQUEST,"장바구니가 비어있습니다."),
-
-    INVALID_MENU_IN_CART(HttpStatus.BAD_REQUEST,"장바구니에 해당 메뉴가 존재하지 않습니다."),
-
     INVALID_TOKEN(HttpStatus.BAD_REQUEST, "일치하는 토큰이 없습니다."),
 
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST,"잘못된 입력값입니다."),
@@ -30,7 +24,15 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러"),
 
     // Custom Exception
-    SECURITY(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다"),;
+    SECURITY(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다"),
+
+    INVALID_AUTHORIZATION(HttpStatus.UNAUTHORIZED,"권한이 없습니다."),
+
+    INVALID_MENU(HttpStatus.BAD_REQUEST,"일치하는 메뉴 정보가 없습니다."),
+
+    INVALID_CART(HttpStatus.BAD_REQUEST,"장바구니가 비어있습니다."),
+
+    INVALID_MENU_IN_CART(HttpStatus.BAD_REQUEST,"장바구니에 해당 메뉴가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
