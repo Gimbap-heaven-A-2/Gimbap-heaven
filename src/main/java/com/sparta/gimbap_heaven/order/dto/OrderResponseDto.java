fbac1a327fb65f914e558ecf45c4c3483b7691de
Kimhyeyun <cqqwer@gmail.com>
2023-12-07 11:57:26 +0900

    refactor: 빌더 사용 시 작성한 애노테이션 제거

--- src/main/java/com/sparta/gimbap_heaven/order/dto/OrderResponseDto.java
+++ src/main/java/com/sparta/gimbap_heaven/order/dto/OrderResponseDto.java
@@ -1,11 +1,8 @@
 package com.sparta.gimbap_heaven.order.dto;
 
-import com.sparta.gimbap_heaven.order.entity.Basket;
 import com.sparta.gimbap_heaven.order.entity.Order;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
